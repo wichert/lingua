c99a24183c88df159d41e9a909a9d91eeef007d4
Wichert Akkerman <wichert@wiggy.net>
2014-02-10 01:49:31 -0600

    Declare Python3 support.

--- setup.py
+++ setup.py
@@ -4,29 +4,33 @@ import sys
 version = '2.0.0dev'
 
 install_requires=[
-        "polib",
+        'polib',
         ]
 if sys.version_info<(2,7):
-    install_requires.append("argparse")
+    install_requires.append('argparse')
 
-setup(name="lingua",
+setup(name='lingua',
       version=version,
-      description="Translation toolset",
+      description='Translation toolset',
       long_description=open('README.rst').read() + '\n' + \
               open('changes.rst').read(),
       classifiers=[
-          "Intended Audience :: Developers",
-          "License :: DFSG approved",
-          "License :: OSI Approved :: BSD License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Topic :: Software Development :: Libraries :: Python Modules",
+          'Intended Audience :: Developers',
+          'License :: DFSG approved',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Libraries :: Python Modules',
           ],
       keywords='translation po gettext Babel',
-      author="Wichert Akkerman",
-      author_email="wichert@wiggy.net",
+      author='Wichert Akkerman',
+      author_email='wichert@wiggy.net',
       url='https://github.com/wichert/lingua',
       license='BSD',
       packages=find_packages('src'),
@@ -34,9 +38,9 @@ setup(name="lingua",
       include_package_data=True,
       zip_safe=True,
       install_requires=install_requires,
-      entry_points="""
+      entry_points='''
       [console_scripts]
       polint = lingua.polint:main
       pot-create = lingua.extract:main
-      """
+      '''
       )
