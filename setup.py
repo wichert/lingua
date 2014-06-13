3bda530dea22d34326f555511793e913d9e6ebd4
Wichert Akkerman <wichert@wiggy.net>
2014-06-13 17:30:59 +0200

    Onwards again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys
 
-version = '2.3'
+version = '2.4dev'
 
 install_requires = [
         'polib',
