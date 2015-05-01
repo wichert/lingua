ea145259e177d3a31d24f84319f064d074e3ba72
Wichert Akkerman <wichert@wiggy.net>
2015-05-01 09:21:05 +0200

    Onwards

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys
 
-version = '3.10'
+version = '3.11.dev.0'
 
 install_requires = [
         'setuptools',
