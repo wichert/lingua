32b44694c291bee0b5db7b07c2aa4f9a3e06305a
wichert-test <wichert@wiggy.net>
2016-10-11 09:49:02 +0200

    Onwards

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys
 
-version = '4.11'
+version = '4.12'
 
 install_requires = [
     'setuptools',
