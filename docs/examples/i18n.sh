b79f4e580e065a56c1cb6aab119385ce8e9c7ba6
Hans-Peter Jansen <hpj@urpla.net>
2017-02-08 14:35:02 +0100

    show language in compilation process

--- docs/examples/i18n.sh
+++ docs/examples/i18n.sh
@@ -35,6 +35,9 @@ if [ $# -eq 0 ]; then
 
     echo "Compile message catalogs"
     for po in "$LOCALES_PATH"/*/LC_MESSAGES/*.po; do
+        lg=${po##$LOCALES_PATH/}
+        lg=${lg%%/LC_MESSAGES/*}
+        echo -n "$lg: "
         msgfmt --statistics -o "${po%.*}.mo" "$po"
     done
 
